da726357f6c2218a19b4cd3455a6cede4d3a688e
Ev Bogue <ev@evbogue.com>
2024-11-02 11:53:48 -0500

    catch on message not having yaml

--- yaml.js
+++ yaml.js
@@ -2,8 +2,12 @@ import { extractYaml } from './lib/frontmatter.js'
 import { parse } from './lib/yaml.js'
 
 export const parseYaml = async (doc) => {
-  const extracted = await extractYaml(doc)
-  const front = await parse(extracted.frontMatter)
-  front.body = extracted.body
-  return front
+  try {
+    const extracted = await extractYaml(doc)
+    const front = await parse(extracted.frontMatter)
+    front.body = extracted.body
+    return front
+  } catch (err) {
+    return { body: doc}
+  }
 }
